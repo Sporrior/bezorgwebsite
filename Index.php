2f01a7c05e98a1e6ecda93cd730a2138a58ddc2e
Sporrior <120579307+Sporrior@users.noreply.github.com>
2023-04-17 11:53:41 +0200

    mappen structuur

--- Index.php
+++ Index.php
@@ -123,8 +123,8 @@
         </div>
     </footer>
 
-    <script src="css/laden.js"></script>
-    <script type="module" src="css/script.js"></script>
+    <script src="bezorgwebsite/css/laden.js"></script>
+    <script type="module" src="bezorgwebsite/css/script.js"></script>
     <script src="https://kit.fontawesome.com/db9de94a89.js" crossorigin="anonymous"></script>
 </body>
 
